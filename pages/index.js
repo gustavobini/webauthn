8534329b503dc0a2079ea5c17622900b7cb5c71a
Gustavo Bini <gustavocb@jmalucelliti.com.br>
2020-02-06 11:25:08 -0300

    requesting webauthn registration

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,104 @@
-import React from 'react';
+import React, { useEffect, useReducer, useRef } from 'react';
 import Head from 'next/head';
 
+const publicKeyChallenge = new Uint8Array([
+  // must be a cryptographically random number sent from a server
+  0x8c,
+  0x0a,
+  0x26,
+  0xff,
+  0x22,
+  0x91,
+  0xc1,
+  0xe9,
+  0xb9,
+  0x4e,
+  0x2e,
+  0x17,
+  0x1a,
+  0x98,
+  0x6a,
+  0x73,
+  0x71,
+  0x9d,
+  0x43,
+  0x48,
+  0xd5,
+  0xa7,
+  0x6a,
+  0x15,
+  0x7e,
+  0x38,
+  0x94,
+  0x52,
+  0x77,
+  0x97,
+  0x0f,
+  0xef
+]).buffer;
+
+const statusEnum = {
+  registeringUser: 'registeringUser',
+  promptingWebAuthn: 'promptingWebAuthn'
+};
+
+const actionEnum = {
+  registrationSubmitted: 'registrationSubmitted'
+};
+
+function reducer(state, action) {
+  switch (state.status) {
+    case statusEnum.registeringUser:
+      if (action.type === actionEnum.registrationSubmitted) {
+        return {
+          ...state,
+          email: action.email,
+          status: statusEnum.promptingWebAuthn
+        };
+      }
+
+    default:
+      return {
+        ...state
+      };
+  }
+}
+
 export default function Main() {
-  React.useEffect(async () => {
+  const inputEmailRef = useRef();
+  const [state, dispatch] = useReducer(reducer, {
+    status: statusEnum.registeringUser
+  });
+
+  const handleWebAuthnRequest = async () => {
     const credential = await navigator.credentials.create({
       publicKey: {
-        challenge
+        challenge: publicKeyChallenge,
+        rp: {
+          name: 'bini webauthn',
+          id: 'webauthn.now.sh'
+        },
+        user: {
+          id: new Uint8Array(16),
+          email: state.email,
+          name: state.email,
+          displayName: state.email.split('@')[0]
+        },
+        pubKeyCredParams: [{ alg: -7, type: 'public-key' }],
+        timeout: 60000,
+        attestation: 'direct'
       }
     });
-  }, []);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    dispatch({
+      type: actionEnum.registrationSubmitted,
+      email: inputEmailRef.current.value
+    });
+  };
 
   return (
     <>
@@ -16,7 +106,23 @@ export default function Main() {
         <title>A WebAuthn experimentation.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        {state.status === statusEnum.registeringUser && (
+          <form onSubmit={handleSubmit}>
+            <h1>Cadastro</h1>
+            <fieldset>
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" ref={inputEmailRef} />
+            </fieldset>
+            <button type="submit">Cadastrar</button>
+          </form>
+        )}
+        {state.status === statusEnum.promptingWebAuthn && (
+          <button type="button" onClick={handleWebAuthnRequest}>
+            Clique aqui para simplificar seu próximo acesso
+          </button>
+        )}
+      </main>
     </>
   );
 }
